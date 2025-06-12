b09a0229f2e7b85deb6a83497c0bbb160191c10b
Victor Barbosa <victoraug09@gmail.com>
2025-06-12 18:27:52 -0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://ijspzmyvcwpgujrkiimc.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlqc3B6bXl2Y3dwZ3VqcmtpaW1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgzODcwMzUsImV4cCI6MjA2Mzk2MzAzNX0.UK9s3yCXDtf6dTqPJLrIgtalhULtUiN7Vio91MoDwEI";
+const SUPABASE_URL = "";
+const SUPABASE_PUBLISHABLE_KEY = "";
 
 // import { supabase } from "@/integrations/supabase/client";
 
